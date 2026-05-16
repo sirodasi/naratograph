c8512ca749689727e014ab3c1e5aa9abd2728b0d
sirodasi <sora1225n@gmail.com>
2026-05-16 15:17:38 +0900

    バトルビューでの呪文と利用可能なリソースの出力を修正

--- src/SessionView.jsx
+++ src/SessionView.jsx
@@ -834,7 +834,7 @@ export function BattleView({ gs, upd, user, isGm, animateDice }) {
     const cardColor   = isPcAttacker ? C.blue : C.red;
     const borderColor = isPcAttacker ? C.blueBorder : C.redBorder;
 
-    console.log(spells, available);
+    console.log(spellsRaw);
 
     // CHOOSE 選択中
     if (b.spellChoose && b.spellChoose.attackerId === (isPcAttacker ? b.pcCombatant : b.npcCombatant)) {
